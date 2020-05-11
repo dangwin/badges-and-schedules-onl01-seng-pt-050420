44862d607acb7b8f6898f6a118d1ac7c0e972e3e
Dang Nguyen <danghwin@gmail.com>
2020-05-11 15:22:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,8 @@
 def badge_maker(name)
-  puts "Hello, my name is #{name}"
-
+  "Hello, my name is #{name}."
 end
-  
+
+def batch_badge_creator(attendees)
+  attendees.collect do |name|
+    badge_maker(name)
+  end
